81c0fe12f3e03fbea5181bcb3b735fc8b5f16b36
Wayne Buxton <waynebuxton2184@yahoo.com>
2017-11-28 16:24:34 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   my_array = [0,1,2,3]
-  my_array.push(4)
+  my_array.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
